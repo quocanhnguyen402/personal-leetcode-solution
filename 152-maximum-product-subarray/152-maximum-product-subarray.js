b7c821a808ea48c608e04e5c380a3710851d4389
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-06-26 00:52:33 +0700

    Time: 260 ms (5.12%), Space: 50.9 MB (5.03%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxProduct = function(nums) {
+    var max = 1;
+    var min = 1;
+    var max_product = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+
+        var tmp_max = max;
+
+        max = Math.max(value*tmp_max,value*min,value);
+        min = Math.min(value*tmp_max,value*min,value);
+
+        console.log(max + " " + min);
+
+        max_product = Math.max(max,max_product);
+    }
+    // console.log(max_product);
+    return max_product;
+};
