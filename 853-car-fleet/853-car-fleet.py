255cef013f7a348ba65844dde34cf7ce4df5c94b
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-07-29 13:54:13 +0700

    Time: 1724 ms (26.30%), Space: 36.4 MB (41.00%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -4,10 +4,9 @@ class Solution:
         pair = [(pos, sped) for pos, sped in zip(position, speed)]
         pair.sort(reverse=True)
         for pos, sped in pair:
-            stack.append([pos,sped])
-            if len(stack) >= 2 and self.getTime(target,stack[-2][1],stack[-2][0]) >= self.getTime(target,stack[-1][1],stack[-1][0]):
+            stack.append(self.getTime(target,sped,pos))
+            if len(stack) >= 2 and stack[-2] >= stack[-1]:
                 stack.pop()
-
         return len(stack)
     def getTime(self,target,speed,pos):
         return (target-pos)/speed
