c36af6937fd32ea4ca00e08dfe552d123331b8ad
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-08-04 15:57:00 +0700

    Time: 80 ms (72.20%), Space: 14 MB (75.51%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -2,31 +2,31 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
+        block_length = 2*numRows-2
         result = [""]*numRows
         index = 0
         while index < len(s):
-            # A block of zig zag numRows = 4 have the length of 2*numRows-2 = 6
+            # A block of zig zag numRows = 4 block_length = 6
             #           distance to the same row
-            # @0          6 but index 6 is not in the block
+            # @0     @6   6 but index 6 is not in the block  
             # @1   @5     4
             # @2 @4       2
             # @3          0
             result[0] += s[index]
             row = 1
-            while 2*numRows-2 - 2*row >= 0:
+            while block_length - 2*row >= 0:
                 first_index = index + row
-                second_index = index + row + 2*numRows-2 - 2*row
+                second_index = index + row + block_length - 2*row
 
                 if first_index != second_index:
                     if first_index < len(s):
-                        result[row] += s[first_index]
+                        result[row] += s[first_index] 
                     if second_index < len(s):
-                        result[row] += s[second_index]
+                        result[row] += s[second_index] 
                 else:
                     if first_index < len(s):
                         result[row] += s[first_index]
                 row += 1
-            index += 2*numRows-2
-
+            index += block_length
+            
         return "".join(result)
-        
