22b2efe6a893899efc7dc9e51a5f27716c1bebb5
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-08-05 15:12:07 +0700

    Time: 2145 ms (58.68%), Space: 14.9 MB (33.77%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,16 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        result = {0:0}
-        for total in range(1,amount+1):
-            result[total] = float('inf')
+        result = {0: 0}
+        for total in range(1, amount+1):
+            result[total] = amount+1
             for coin in coins:
                 if total - coin == 0:
                     result[total] = 1
                 else:
                     if total - coin in result:
-                        result[total] = min(result[total],result[total-coin] + 1)
-            
-        if result[amount] == float('inf'):
+                        result[total] = min(
+                            result[total], result[total-coin] + 1)
+
+        if result[amount] == amount+1:
             return -1
         else:
             return result[amount]
