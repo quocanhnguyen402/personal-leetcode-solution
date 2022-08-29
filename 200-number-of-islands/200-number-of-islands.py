b5a9be955d57188324d31248f5c5b5cbbf15322e
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-08-29 11:32:25 +0700

    Time: 416 ms (70.69%), Space: 16.5 MB (65.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
-        trace = [[1]*col for _ in range(row)]
         result = 0
-        def findAdjacentLand(pos_row, pos_col, trace):
-            if trace[pos_row][pos_col] == 1 and grid[pos_row][pos_col] == "1":
-                trace[pos_row][pos_col] = 0
+        def findAdjacentLand(pos_row, pos_col):
+            if grid[pos_row][pos_col] == "1":
+                grid[pos_row][pos_col] = "-1"
                 if pos_row - 1 >= 0:
-                    findAdjacentLand(pos_row-1, pos_col, trace)
+                    findAdjacentLand(pos_row-1, pos_col)
                 if pos_row + 1 < row:
-                    findAdjacentLand(pos_row+1, pos_col, trace)
+                    findAdjacentLand(pos_row+1, pos_col)
                 if pos_col - 1 >= 0:
-                    findAdjacentLand(pos_row, pos_col-1, trace)
+                    findAdjacentLand(pos_row, pos_col-1)
                 if pos_col + 1 < col:
-                    findAdjacentLand(pos_row, pos_col+1, trace)
+                    findAdjacentLand(pos_row, pos_col+1)
         for i in range(row):
             for j in range(col):
-                if trace[i][j] == 1 and grid[i][j] == "1":
+                if grid[i][j] == "1":
                     result += 1
-                    findAdjacentLand(i, j, trace)
+                    findAdjacentLand(i, j)
         return result
