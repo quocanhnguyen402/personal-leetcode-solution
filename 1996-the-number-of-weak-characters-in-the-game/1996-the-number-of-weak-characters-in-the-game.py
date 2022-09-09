e1eacb7a24c91101954ffef097c9d591dc6c4169
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-09-09 14:21:47 +0700

    Time: 5572 ms (5.04%), Space: 70.9 MB (7.43%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]):
+        properties.sort(reverse=True)
+        set_prop = {}
+        for attack,defend in properties:
+            if attack not in set_prop:
+                set_prop[attack] = [defend]
+            else:
+                set_prop[attack].append(defend)
+        max_def = -1
+        result = 0
+        for attack in set_prop:
+            defends = set_prop[attack]
+            max_def_temp = max_def
+            for defend in defends:
+                if defend < max_def:
+                    result += 1
+                max_def_temp = max(max_def_temp,defend)
+            max_def = max_def_temp
+        return result
+        
