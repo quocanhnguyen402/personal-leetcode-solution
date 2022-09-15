53214987fc38ba796680f5ee6311b8ed1ea21ea7
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-09-15 11:30:05 +0700

    Time: 3887 ms (5.13%), Space: 32.8 MB (33.24%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 != 0:
+            return []
+        
+        map = {}
+        for i in range(len(changed)):
+            if changed[i] not in map:
+                map[changed[i]] = 1
+            else:
+                map[changed[i]] += 1
+                
+        half = len(changed) // 2
+        changed.sort()
+        result = []
+        for i in range(len(changed)):
+            val = changed[i]
+            if map[val] != 0:
+                if val * 2 in map and map[val * 2] > 0:
+                    map[val] -= 1
+                    map[val*2] -= 1
+                    result.append(val)
+        for i in map:
+            if map[i] > 0:
+                return []
+        return result if len(result) == half else []
