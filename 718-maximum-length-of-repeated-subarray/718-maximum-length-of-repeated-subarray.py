b8addf99e98f264e725eeac8c5bb670a81b7635d
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2022-09-20 21:24:44 +0700

    Time: 9024 ms (8.48%), Space: 39.3 MB (50.31%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,8 +1,16 @@
 class Solution(object):
-    def findLength(self, A, B):
-        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
-        for i in range(len(A) - 1, -1, -1):
-            for j in range(len(B) - 1, -1, -1):
-                if A[i] == B[j]:
-                    memo[i][j] = memo[i + 1][j + 1] + 1
-        return max(max(row) for row in memo)
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        len1 = len(nums1)
+        len2 = len(nums2)
+        max_len = 0
+        # nums1 is row
+        # nums2 is column
+        trace  = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+        for i in range(1,len1+1):
+            for j in range(1,len2+1):
+                if nums1[i-1] == nums2[j-1]:
+                    trace[i][j] = trace[i-1][j-1] + 1
+                    max_len = max(trace[i][j],max_len)
+                else:
+                    trace[i][j] = 0
+        return max(max(row) for row in trace)
