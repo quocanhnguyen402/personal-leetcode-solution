98543d591795bdea428774c208cc1e9a8417e111
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2023-09-20 14:43:41 +0700

    Time: 933 ms (88.98%), Space: 30.3 MB (83.06%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,12 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        res = float("inf")
+        
 
         left = 0
         cur_sum = 0
         n = len(nums)
-
         target = sum(nums) - x
+        res = n + 1
+        
         if target < 0:
             return -1
         if target == 0:
@@ -22,4 +23,4 @@ class Solution:
             if cur_sum == target:
                 res = min(res, n - (right - left + 1))
 
-        return -1 if res == float("inf") else res
+        return -1 if res == n + 1 else res
