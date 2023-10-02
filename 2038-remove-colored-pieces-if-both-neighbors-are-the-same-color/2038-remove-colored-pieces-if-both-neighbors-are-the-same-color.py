51cc5386f63bbbeff0336f77b215acb42ef8ba21
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2023-10-02 15:03:37 +0700

    Time: 214 ms (45.60%), Space: 17.2 MB (94.09%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -2,11 +2,9 @@ class Solution:
     def winnerOfGame(self, colors: str) -> bool:
         moved = [0,0]
         for i in range(1,len(colors)-1):
-            char = colors[i]
-            if char == colors[i-1] == colors[i+1]:
-                if char == 'A':
+            if colors[i] == colors[i-1] == colors[i+1]:
+                if colors[i] == 'A':
                     moved[0] += 1
                 else:
                     moved[1] += 1
-        
         return moved[0] - moved[1] > 0
