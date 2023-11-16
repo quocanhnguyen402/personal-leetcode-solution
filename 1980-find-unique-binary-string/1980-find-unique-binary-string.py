1c7618397be0df086a2495a1a22df051db4cab1b
quocanhnguyen <43027577+quocanhnguyen402@users.noreply.github.com>
2023-11-16 13:46:50 +0700

    Time: 47 ms (31.34%), Space: 16.5 MB (14.04%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -12,8 +12,6 @@ class Solution:
                 a = findBinary(string + "0", n)
                 if (a):
                     return a
-                b = findBinary(string + "1", n)
-                if (b):
-                    return b
+                return findBinary(string + "1", n)
 
         return findBinary("", len(nums))
